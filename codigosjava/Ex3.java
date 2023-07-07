5667d2460b7206047c61aee88cd893487e74f6dd
Diogo Gabardo <thenolance10@gmail.com>
2023-07-07 20:48:08 -0300

    Ex3 resolvido

--- codigosjava/Ex3.java
+++ codigosjava/Ex3.java
@@ -2,19 +2,24 @@ public class Ex3 {
 
 	public static void main(String[] args) {
 		String palavra = "babad";
-		
-		for(int i = 0; i < palavra.length(); i++) {
-			
-			char letraAtual = palavra.charAt(i);
-			char letraProx = palavra.charAt(i+1);
-			boolean palindromo = true;
-			String palavraInvertida = "";
-			String palavraBase = "";
-		}
-		
-		
-		
+		String palindromo = "";
+        int tamanho = palavra.length()-1;
+        
+        for(int i = 0; i <= tamanho; i++) {
+        	palindromo += palavra.charAt(i);
+        	if(i > 1) {  // para imprimir apenas palindromos com 3 caracteres ou mais
+        		String palavraBase = palindromo;
+        		String palavraInvertida = "";
+        		for(int j = palindromo.length()-1; j >= 0; j--) { // laço secundario percorrendo a string palidromo de tras pra frente
+                    palavraInvertida += palindromo.charAt(j);
+                }
+        		if (palavraBase.equals(palavraInvertida)) {
+                    System.out.println(palindromo);
+            }
+        	}
+        }
 		
 	}
 
 }
+
