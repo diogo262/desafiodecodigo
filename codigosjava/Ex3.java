2b25127ce1d6930d3810b7a909e9fe72b3d5b6c2
Diogo Gabardo <thenolance10@gmail.com>
2023-07-07 19:56:52 -0300

    Inicio do Ex3

---
+++ codigosjava/Ex3.java
@@ -0,0 +1,20 @@
+public class Ex3 {
+
+	public static void main(String[] args) {
+		String palavra = "babad";
+		
+		for(int i = 0; i < palavra.length(); i++) {
+			
+			char letraAtual = palavra.charAt(i);
+			char letraProx = palavra.charAt(i+1);
+			boolean palindromo = true;
+			String palavraInvertida = "";
+			String palavraBase = "";
+		}
+		
+		
+		
+		
+	}
+
+}
